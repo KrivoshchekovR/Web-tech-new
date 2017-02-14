2b129cdbb943779fc761de78c00968860e37bfeb
ctok81 <savin-m-s-a@yandex.ru>
2017-02-14 11:04:17 +0500

    +css

--- index.php
+++ index.php
@@ -9,8 +9,44 @@
 	<meta name="description" content="Здесь нужен текст.">
 
 	<meta name="keywords" content="помощь, help,html, css, js, php, ">
+
+	<style>
+		div {
+			display: block;
+			background-color: #432435;
+            width: 100%;
+		}
+		ul.nav {
+			margin: 0; 
+			padding: 6px; 
+			 }
+		 
+		ul.nav li {
+			display: inline-table;
+			margin-right: 5px;
+			border-radius: 20px;
+			padding: 3px; 
+			}
+			
+		li a {
+			text-decoration: none;
+			font-family: 'Arial';
+			font-size: 12;
+			color: #72638B;
+			}
+			
+		a:hover, a:active { 
+			font-weight: bold;
+			}
+	</style>
   </head>
-  <body>
-    
+  <body margin: 0px>
+    <div class="navigation">
+		<ul class="nav">
+			<li><a href="#">Главная страница</a></li>
+			<li><a href="#">Заказать помощь</a></li>
+			<li><a href="#">Виды услуг</a></li>
+		</ul>
+	</div>
   </body>
 </html>
