5ed3cd89a6651706db44676e41627a0ac4c6bce3
ctok81 <savin-m-s-a@yandex.ru>
2017-02-14 10:00:57 +0500

    Revert "Revert "+head""
    This reverts commit a7e50f601d19432f3d7beb372cec2a4392ea5c3d.

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
-<html> 
-  <head></head>
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+	<title>Быстрая компьютерная помощь на дому.</title>
+	<meta charset="utf-8">
+	<meta name="author" lang="ru" content="Максим Савин">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<meta name='yandex-verification' content='60735ae38dd9271d' />	
+	<meta name="description" content="Здесь нужен текст.">
+
+	<meta name="keywords" content="помощь, help,html, css, js, php, ">
+  </head>
   <body>
-    <?php echo "Hello World" ?>
+    
   </body>
 </html>
