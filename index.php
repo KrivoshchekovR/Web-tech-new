81e9bad1ee1ce3f6b7f4320c4fcef55840a1e684
ctok81 <savin-m-s-a@yandex.ru>
2017-02-28 08:48:27 +0500

    Update index.php

--- index.php
+++ index.php
@@ -5,14 +5,12 @@
 	</head>
 	<body>
 	<header>  
-	
-	</header>
-	
 		<nav align=center>
 			<ul><a href="index.php">Главная страница</a></ul>
 			<ul><a href="contact.php">Контакты</a></ul>
 			<ul><a href="zakaz.php">Заказать услугу</a></ul>
 		</nav>
+	</header>
 		
 	<article>
 	
